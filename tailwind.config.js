80b477f4264459637052202a6cde41fe84107319
lyndonpett <lyndonpett98@gmail.com>
2022-10-12 15:30:05 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'custom-blue': '#83d4ff',
+                'custom-pink': '#d97aee',
+                'custom-sky': 'hsl(223, 90%, 65%)',
+            },
+        },
+    },
+    plugins: [],
 };
